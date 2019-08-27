371419d6007f454c3366d94d906e98bbaaa6950a
Clayton <herbsca1@gmail.com>
2019-08-27 23:50:58 +0800

    setting up node project, python virtual environment and django project template

---
+++ web-app/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/herbsca/OneDrive/UWA/CITS3200/cits3200-db/f.o.d/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
