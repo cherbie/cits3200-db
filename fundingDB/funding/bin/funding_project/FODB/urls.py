8bd9affbd1d6dd54636443429bb399b5f15ef223
Aaymen <aaymenk@gmail.com>
2019-08-28 19:57:41 +0800

    tesy

---
+++ fundingDB/funding/bin/funding_project/FODB/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='homepage'),
+    path('about/', views.about, name='FODB-about'),
+]
+
---
+++ fundingDB/funding/bin/funding_project/FODB/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+	return HttpResponse('<h1>FDOB Home</h1>')
+
+
+def about(request):
+	return HttpResponse('<h1>FDOB about</h1>')
