ff5ce4e700d6c30275fe603a1866f6a894d60ce4
Aaymen <aaymenk@gmail.com>
2019-08-30 13:44:53 +0800

    Updatated login and logout URLS
    changed the views to make sure that only those authenticated can see the post and logout button

--- fundingDB/funding/bin/funding_project/funding_project/settings.py
+++ fundingDB/funding/bin/funding_project/funding_project/settings.py
@@ -31,11 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-
-    'crispy_forms',
     'fodb.apps.FodbConfig',
     'users.apps.UsersConfig',
-    
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,5 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'fodb-home'
 
 
