921cf0fac3224b1caa7d463d4dd5394594afc036
Clayton <herbsca1@gmail.com>
2019-09-01 17:46:50 +0800

    initial routing error handling implemented

--- funding-db/funding_project/fodb/urls.py
+++ funding-db/funding_project/fodb/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='fodb-home'),
-    path('about/', views.about, name='fodb-about')
+    path('', views.opportunities, name='fodb-home'),
+    # path('about', views.about, name='fodb-about'),
+    re_path(r'error', views.error, name='fodb-error'),
+    re_path(r'.*', views.unknown, name='fodb-unknown') # redirects to project error page if no match
 ]
-
--- funding-db/funding_project/fodb/views.py
+++ funding-db/funding_project/fodb/views.py
@@ -1,22 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 
 # Create your views here.
-# This is where the routes are held 
+# This is where the routes are held
 
-#or make this the welcome page?
-def home(request):
+
+def opportunities(request):
+	'''
+		Display the list of database entries.
+		Render the main.html template.
+	'''
 	context = {
 		'posts': Post.objects.all()
 	}
-	return render(request,'fodb/home.html', context)
+	return render(request,'fodb/opportunities.html', context)
 
 
+# you can change this to the welcome page?
+# def about(request):
+# 	return render(request,'fodb/about.html', {'title':'About'})
 
-#you can change this to the welcome page?
-def about(request):
-	return render(request,'fodb/about.html', {'title':'About'})
+def error(request, error=None):
+	'''
+		Render the error template if an error has occured.
+	'''
+	if error == None:
+		error = {
+			'title': 'Unexpected Error',
+			'status_code': 404,
+			'message': 'error message.'
+		}
+	return render(request, 'fodb/error.html', {'error':error})
 
 
-
+def unknown(request):
+	'''
+		Redirects to controlled error page.
+	'''
+	return redirect('/error')
