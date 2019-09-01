2c52ae425c1a3a5390e8cf416b290ac08d8a990f
Clayton <herbsca1@gmail.com>
2019-09-01 17:48:02 +0800

    deleted all references to 'fodb/about.html'

--- funding-db/funding_project/fodb/urls.py
+++ funding-db/funding_project/fodb/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.opportunities, name='fodb-home'),
-    # path('about', views.about, name='fodb-about'),
     re_path(r'error', views.error, name='fodb-error'),
     re_path(r'.*', views.unknown, name='fodb-unknown') # redirects to project error page if no match
 ]
