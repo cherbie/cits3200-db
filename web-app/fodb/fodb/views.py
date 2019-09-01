3d8ab549e58b3b7a929f8ac29a2ebe78380d59d3
Clayton <herbsca1@gmail.com>
2019-09-01 09:56:38 +0800

    superseded initial funding opportunities django-project

--- web-app/fodb/fodb/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def welcome_page(request):
-    return HttpResponse('Welcome to the funding opportunities database.');
--- web-app/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/herbsca/OneDrive/UWA/CITS3200/cits3200-db/f.o.d/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
