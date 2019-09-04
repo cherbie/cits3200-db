e082efb975c622fbbde0ac510aaf212f6381a9f9
Clayton <herbsca1@gmail.com>
2019-09-04 15:19:01 +0800

    reversing changes

--- funding-db/funding_project/funding_project/urls.py
+++ funding-db/funding_project/funding_project/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
-keepinhg the login and logout templates in user app 
+keepinhg the login and logout templates in user app
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -23,12 +23,10 @@ from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('fodb.urls')),
 ]
-
-
