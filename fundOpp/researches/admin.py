128ceae6e9071312eae067aa35107951f5886caa
Gavin <gavinvietnguyen2710@gmail.com>
2019-09-05 22:06:08 +0800

    Attempt working on database interface

--- fundOpp/researches/admin.py
+++ fundOpp/researches/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Funding_Opportunity1, ImportantDate, Boolean_Tags, Funding
+
+admin.site.register(Funding_Opportunity1)
+admin.site.register(ImportantDate)
+admin.site.register(Boolean_Tags)
+admin.site.register(Funding)
