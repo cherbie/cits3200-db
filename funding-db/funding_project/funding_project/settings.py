e581c0949d0c624db190e1604470b9aba47a0286
Aaymen <aaymenk@gmail.com>
2019-09-08 14:40:54 +0800

    changed requirements

--- funding-db/funding_project/funding_project/settings.py
+++ funding-db/funding_project/funding_project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'fodb.apps.FodbConfig',
     'users.apps.UsersConfig',
-    # 'crispy_forms',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
