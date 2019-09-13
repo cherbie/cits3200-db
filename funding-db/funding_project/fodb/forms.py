773ff0d807bc14cf4a1869a07bd04bd8262243aa
Clayton <herbsca1@gmail.com>
2019-09-13 14:07:27 +0800

    improvements made to filter

--- funding-db/funding_project/fodb/forms.py
+++ funding-db/funding_project/fodb/forms.py
@@ -6,12 +6,12 @@ class FilterForm(forms.Form):
         Note: The QuerySet generation logic has been seperated from the forms class
         This form only provides the structure requirements
     '''
-    ecr = forms.BooleanField(required=False)
-    travel = forms.BooleanField(required=False, initial="False")
-    visiting = forms.BooleanField(required=False, initial="False")
-    wir = forms.BooleanField(required=False, initial="False")
-    phd = forms.BooleanField(required=False, initial="False")
-    international = forms.BooleanField(required=False, initial="False")
-    hms = forms.BooleanField(required=False, initial="False")
-    ems = forms.BooleanField(required=False, initial="False")
-    science = forms.BooleanField(required=False, initial="False")
+    ecr = forms.BooleanField(required=False, initial='False', label='ECR')
+    travel = forms.BooleanField(required=False, initial='False', label='Travel')
+    visiting = forms.BooleanField(required=False, initial='False', label='Visiting')
+    wir = forms.BooleanField(required=False, initial='False', label='Women in Research')
+    phd = forms.BooleanField(required=False, initial='False', label='PhD')
+    international = forms.BooleanField(required=False, initial='False', label='International')
+    hms = forms.BooleanField(required=False, initial='False', label='HMS')
+    ems = forms.BooleanField(required=False, initial='False', label='EMS')
+    science = forms.BooleanField(required=False, initial='False', label='Science')
