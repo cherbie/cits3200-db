26b8f9bf7cb37cd4b3f67d3b020cc9f95ac2c319
okokok-ok <flender1912@gmail.com>
2019-09-17 21:09:38 +0800

    Update admin.py

--- funding-db/funding_project/fodb/admin.py
+++ funding-db/funding_project/fodb/admin.py
@@ -15,7 +15,7 @@ admin.site.site_header = 'Funding Opportunities Database'
 
 class FundingOpportunityAdmin(admin.ModelAdmin):
 	fieldsets = [
-        ('Funding Opportunity',{'fields': [('name','is_hidden'),'description','link','herdc','limit_per_uni']}),
+        ('Funding Opportunity',{'fields': [('name','is_hidden'),'provider','description','link','herdc','limit_per_uni']}),
         ('Date Information', {'fields': [('closing_date', 'External_deadline','Internal_deadline'),('EOI_deadline','Minimum_data_deadline')]}),
         ('Amount and Duration', {'fields': [('max_amount','amount_estimated'),('max_duration','duration_estimated') ,'duration_type']}),
         ('Tags', {'fields': ['ecr','travel','visiting','wir','phd','international','hms','ems','science' ]}),
