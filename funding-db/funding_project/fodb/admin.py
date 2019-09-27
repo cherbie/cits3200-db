0642c4cbb83c3827f133ac06ef7b1ba979c95814
okokok-ok <flender1912@gmail.com>
2019-09-27 14:18:50 +0800

    update

--- funding-db/funding_project/fodb/admin.py
+++ funding-db/funding_project/fodb/admin.py
@@ -11,18 +11,14 @@ from django.utils.html import format_html
 
 admin.site.site_header = 'Funding Opportunities Database'
 
-class important_dateInline(admin.TabularInline):
-    model = important_date
-    extra = 1
 
 class FundingOpportunityAdmin(admin.ModelAdmin):
 	fieldsets = [
-        ('Funding Opportunity',{'fields': ['name','description','link','herdc','limit_per_uni']}),
-        ('Date Information', {'fields': ['closing_month']}),
+        ('Funding Opportunity',{'fields': ['name','description','link','limit_per_uni']}),
+        ('Date Information', {'fields': ['closing_month','Internal_deadline'], ['EOI_deadline','Minimum_data_deadline'],['External_deadline','Forecast_Month']}),
         ('Amount and Duration', {'fields': ['max_amount','amount_estimated','max_duration','duration_type','duration_estimated']}),
         ('Tags', {'fields': ['ecr','travel','visiting','wir','phd','international','hms','ems','science','is_hidden' ]}),
     ]
-	inlines = [important_dateInline]
 	list_filter = ('closing_month', )
 	list_display = ('name', 'closing_month','max_amount','max_duration')
 	search_fields = ['description','name']
--- funding-db/funding_project/fodb/models.py
+++ funding-db/funding_project/fodb/models.py
@@ -8,12 +8,11 @@ import datetime
 
 class funding_opportunity(models.Model):
 	Year_or_Month =( ('Y','year'), ('M','month'),)
-	Herdc_type = (('1','category1'),('2','category2'),('3','category3'),('4','category4'),)
+	Forecast_Mon = (('Jan'), ('Feb'), ('Mar'), ('Apr'), ('May'), ('Jun'), ('Jul'), ('Aug'), ('Sep'), ('Oct'), ('Nov'), ('Dec'),)
 
 	provider = models.CharField(max_length = 100, verbose_name = 'Funding provider')
 	name = models.CharField(max_length = 100, verbose_name = 'Title')
 	description = models.CharField()
-	herdc = models.CharField(blank = True, max_length = 15,  choices = Herdc_type)
 	closing_month = models.DateTimeField(null = False)
 	creation_date = models.DateField(auto_now_add = True)
 	last_updated = models.DateField(auto_now= True)
@@ -32,13 +31,15 @@ class funding_opportunity(models.Model):
 	wir = models.BooleanField(default = False)
 	phd = models.BooleanField(default = False)
 	international = models.BooleanField(default = False)
-	hms = models.BooleanField(default = False)
-	ems = models.BooleanField(default = False)
-	science = models.BooleanField(default = False)
+	hms = models.BooleanField(default = False, verbose_name = 'HMS')
+	ems = models.BooleanField(default = False, verbose_name = 'EMS')
+	science = models.BooleanField(default = False, verbose_name = 'SCI')
+	fable = models.BooleanField(default = False, verbose_name = 'FABLE')
 
 	is_hidden = models.BooleanField(default = False)
 
 	External_deadline = models.DateTimeField(blank = True, null = True)
+	Forecast_Month = models.CharField(blank = True, max_length = 15,  choices = Forecast_Mon)
 	Internal_deadline = models.DateTimeField(blank = True, null = True)
 	EOI_deadline  = models.DateTimeField(blank = True, null = True, verbose_name = 'Expression of interest deadline')
 	Minimum_data_deadline = models.DateTimeField(blank = True, null = True)
