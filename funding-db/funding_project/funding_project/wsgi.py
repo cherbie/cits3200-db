94f4dd1070ba6487cda705c913bc8cab3d8f38fe
Etrenix <ryukuzui@gmail.com>
2019-09-29 04:51:20 +0800

    trying to fix path to funding_project.settings

--- funding-db/funding_project/funding_project/wsgi.py
+++ funding-db/funding_project/funding_project/wsgi.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 # AWS deployment test
 import sys
-path = '/home/ubuntu/django/cits3200-db/funding-db'
+path = '/home/ubuntu/django/cits3200-db/funding-db/funding_project'
 if path not in sys.path:
     sys.path.append(path)
-    
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funding_project.settings')
