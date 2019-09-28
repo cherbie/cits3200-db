81082f3876f448bb76d6c263a0d6ad65edc7f74a
Etrenix <ryukuzui@gmail.com>
2019-09-28 13:19:06 +0800

    fixed mixed tabs and spaces in admin.py

--- funding-db/funding_project/fodb/admin.py
+++ funding-db/funding_project/fodb/admin.py
@@ -14,15 +14,15 @@ admin.site.site_header = 'Funding Opportunities Database'
 
 
 class FundingOpportunityAdmin(admin.ModelAdmin):
-	fieldsets = [
+    fieldsets = [
         ('Funding Opportunity',{'fields': [('name','is_hidden'),'provider','description','link','herdc','limit_per_uni']}),
         ('Date Information', {'fields': [('closing_date', 'External_deadline','Internal_deadline'),('EOI_deadline','Minimum_data_deadline')]}),
         ('Amount and Duration', {'fields': [('max_amount','amount_estimated'),('max_duration','duration_estimated') ,'duration_type']}),
         ('Tags', {'fields': ['ecr','travel','visiting','wir','phd','international','hms','ems','science' ]}),
     ]
-	list_filter = ('closing_date', )
-	list_display = ('name', 'closing_date','max_amount','max_duration', 'is_hidden')
-	search_fields = ['description','name']
+    list_filter = ('closing_date', )
+    list_display = ('name', 'closing_date','max_amount','max_duration', 'is_hidden')
+    search_fields = ['description','name']
 
 
     # this changes the title at /admin/fodb/funding_opportunity/ 
