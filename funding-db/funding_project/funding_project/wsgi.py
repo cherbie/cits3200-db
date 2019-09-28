6187c2f924abd6bbeef1a462f1739c92450850b6
Etrenix <ryukuzui@gmail.com>
2019-09-29 04:49:24 +0800

    changing wsgi.py for AWS

--- funding-db/funding_project/funding_project/wsgi.py
+++ funding-db/funding_project/funding_project/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+# AWS deployment test
+import sys
+path = '/home/ubuntu/django/cits3200-db/funding-db'
+if path not in sys.path:
+    sys.path.append(path)
+    
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funding_project.settings')
