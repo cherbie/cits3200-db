fd05fe4cbfc766334e2962faa786c995a8226299
Etrenix <ryukuzui@gmail.com>
2019-09-29 23:33:42 +0800

    added new domain name uwa-fodb.ml

--- funding-db/funding_project/funding_project/settings.py
+++ funding-db/funding_project/funding_project/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = '#83vf$qugojs09%cg!ygs=p=@c=&8)-1#l^5(pflgq5=s0*xy!'
 DEBUG = True
 
 # For AWS deployment
-ALLOWED_HOSTS = ['localhost', 'ec2-13-211-152-223.ap-southeast-2.compute.amazonaws.com' , '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 
+                 'ec2-13-211-152-223.ap-southeast-2.compute.amazonaws.com', 'uwa-fundop.ml']
 
 # For AWS deployment
 SETTINGS_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
