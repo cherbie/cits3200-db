05d811671b19a1722fdf7943b9dd837e29f41f49
Etrenix <ryukuzui@gmail.com>
2019-10-04 02:03:00 +0800

    registration with email authentication working (not fully tested yet)

--- funding-db/funding_project/funding_project/urls.py
+++ funding-db/funding_project/funding_project/urls.py
@@ -22,11 +22,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
+
+# Auth test 2
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    
+    # Auth test 2
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        user_views.activate, name='activate'),
     path('', include('fodb.urls')),
 ]
 
--- funding-db/funding_project/users/views.py
+++ funding-db/funding_project/users/views.py
@@ -3,6 +3,18 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
+# Auth test 2
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from .forms import UserRegisterForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
 # Create your views here.
 #if it is just a post request then just display then check if it is valid and display the name
 
@@ -10,10 +22,23 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You are now able to log in')
-            return redirect('login')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
@@ -21,5 +46,21 @@ def register(request):
 @login_required
 def profile(request):
     return render(request,'users/profile.html')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
 
 
