d3232b9b2a858f373fba4873fdfc0e3ae0e1b4c6
Aaymen <aaymenk@gmail.com>
2019-10-13 10:39:35 +0800

    updated impor/export

--- funding-db/funding_project/fodb/urls.py
+++ funding-db/funding_project/fodb/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     re_path(r'^(add)|(edit)', views.db_update, name='fodb-db-update'),
     re_path(r'error', views.error, name='fodb-error'),
     re_path(r'.*', views.unknown, name='fodb-unknown'), # redirects to project error page if no match
+
 ]
